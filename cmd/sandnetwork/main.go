00f278531cc4f4e1fe96e9dd348f131d914ed9b4
Frédéric VIRICEL <99799464+xxxPOUPOUxxx@users.noreply.github.com>
2025-10-19 22:08:57 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/sandnetwork/main.go
@@ -0,0 +1,23 @@
+// cmd/sandnetwork/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "sandnetwork/internal/sandnetwork"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := sandnetwork.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
